8daa6f2e1a6a7e48c60f9b5a15195e6f4d512836
filippovillani <filippo.villani.1996@gmail.com>
2022-12-04 18:05:11 +0100

    model architecture implemented - to be tested

--- model.py
+++ model.py
@@ -1,42 +1,5 @@
 import torch
 import torch.nn as nn
-def unet(input_size=(96,248,1)):
-    inputs = tf.keras.layers.Input(input_size)
-    
-    # Contracting path
-    conv1 = tf.keras.layers.Conv2D(64, 3, padding='same', kernel_initializer='he_normal')(inputs)
-    batch1 = tf.keras.layers.BatchNormalization()(conv1)
-    act1 = tf.keras.layers.Activation('relu')(batch1)
-    pool1 = tf.keras.layers.MaxPool2D()(act1) # default size is (2,2)
-
-    conv2 = tf.keras.layers.Conv2D(128, 3, padding='same', kernel_initializer='he_normal')(pool1)
-    batch2 = tf.keras.layers.BatchNormalization()(conv2) 
-    act2 = tf.keras.layers.Activation('relu')(batch2)
-    drop2 = tf.keras.layers.Dropout(0.3)(act2)
-    pool2 = tf.keras.layers.MaxPool2D()(drop2)
-    
-    conv3 = tf.keras.layers.Conv2D(256, 3, padding='same', kernel_initializer='he_normal')(pool2)
-    batch3 = tf.keras.layers.BatchNormalization()(conv3) 
-    act3 = tf.keras.layers.Activation('relu')(batch3)
-    drop3 = tf.keras.layers.Dropout(0.3)(act3)
-
-    #Expanding path
-    upsamp4 = tf.keras.layers.UpSampling2D()(drop3) # default size is (2,2)
-    concat4 = tf.concat([act2, upsamp4], axis=3)
-    conv4 = tf.keras.layers.Conv2D(128, 3, padding='same', kernel_initializer='he_normal')(concat4)
-    batch4 = tf.keras.layers.BatchNormalization()(conv4) 
-    act4 = tf.keras.layers.Activation('relu')(batch4)
-
-
-    upsamp5 = tf.keras.layers.UpSampling2D()(act4)
-    concat5 = tf.concat([act1, upsamp5], axis=3)
-    conv5 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(concat5)
-    conv5 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
-    conv5 = tf.keras.layers.Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
-    output = tf.keras.layers.Conv2D(1, 1, activation='sigmoid')(conv5)
-
-    model = tf.keras.Model(inputs = inputs, outputs = output)
-    return model
 
 class UNet(nn.Module):
     def __init__(self):
@@ -44,6 +7,7 @@ class UNet(nn.Module):
         # Contracting path      
         self.convC11 = nn.Conv2d(1, 64, 3, padding='same')
         nn.init.kaiming_normal_(self.convC11.weight)
+        self.bnC11 = nn.BatchNorm2d(64)        
         self.reluC11 = nn.ReLU() 
         self.convC12 = nn.Conv2d(64, 64, 3, padding='same')
         nn.init.kaiming_normal_(self.convC12.weight)
@@ -54,6 +18,7 @@ class UNet(nn.Module):
 
         self.convC21 = nn.Conv2d(64, 128, 3, padding='same')
         nn.init.kaiming_normal_(self.convC21.weight)
+        self.bnC21 = nn.BatchNorm2d(128)        
         self.reluC21 = nn.ReLU() 
         self.convC22 = nn.Conv2d(128, 128, 3, padding='same')
         nn.init.kaiming_normal_(self.convC22.weight)
@@ -76,16 +41,37 @@ class UNet(nn.Module):
         self.convC41 = nn.Conv2d(256, 512, 3, padding='same')
         nn.init.kaiming_normal_(self.convC41.weight)
         self.reluC41 = nn.ReLU() 
-        self.convC42 = nn.Conv2d(512, 256, 3, padding='same')
+        self.convC42 = nn.Conv2d(512, 512, 3, padding='same')
         nn.init.kaiming_normal_(self.convC42.weight)
-        self.bnC42 = nn.BatchNorm2d(256)
+        self.bnC42 = nn.BatchNorm2d(512)
         self.reluC42 = nn.ReLU() 
         self.dropC4 = nn.Dropout(0.3)
-        
+        self.poolC4 = nn.MaxPool2d(kernel_size=2)
+  
+        self.convC51 = nn.Conv2d(512, 1024, 3, padding='same')
+        nn.init.kaiming_normal_(self.convC51.weight)
+        self.bnC51 = nn.BatchNorm2d(512)
+        self.reluC51 = nn.ReLU() 
+        self.convC52 = nn.Conv2d(1024, 512, 3, padding='same')
+        nn.init.kaiming_normal_(self.convC52.weight)
+        self.bnC52 = nn.BatchNorm2d(512)
+        self.reluC52 = nn.ReLU() 
+        self.dropC5 = nn.Dropout(0.3)
         
         #Expanding path
+        self.upsamp4 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.convE41 = nn.Conv2d(1024, 512, 3, padding='same')
+        nn.init.kaiming_normal_(self.convE41.weight)
+        self.bnE41 = nn.BatchNorm2d(512)
+        self.reluE41 = nn.ReLU() 
+        self.convE42 = nn.Conv2d(512, 512, 3, padding='same')
+        nn.init.kaiming_normal_(self.convE42.weight)
+        self.bnE42 = nn.BatchNorm2d(512)
+        self.reluE42 = nn.ReLU() 
+        self.dropE4 = nn.Dropout(0.3)
+        
         self.upsamp3 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.convE31 = nn.Conv2d(1024, 256, 3, padding='same')
+        self.convE31 = nn.Conv2d(512, 256, 3, padding='same')
         nn.init.kaiming_normal_(self.convE31.weight)
         self.bnE31 = nn.BatchNorm2d(256)
         self.reluE31 = nn.ReLU() 
@@ -93,86 +79,131 @@ class UNet(nn.Module):
         nn.init.kaiming_normal_(self.convE32.weight)
         self.bnE32 = nn.BatchNorm2d(256)
         self.reluE32 = nn.ReLU() 
-
+        self.dropE3 = nn.Dropout(0.3)
+        
         self.upsamp2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.upconvE2 = nn.Conv2d(512, 128, 2, padding='same')
-        nn.init.kaiming_normal_(self.upconvE2.weight)
-        self.bnE21 = nn.BatchNorm2d(128)
-        self.upreluE2 = nn.ReLU() 
-        self.convE21 = nn.Conv2d(128, 128, 3, padding='same')
+        self.convE21 = nn.Conv2d(256, 128, 3, padding='same')
         nn.init.kaiming_normal_(self.convE21.weight)
-        self.reluE21 = nn.ReLU() 
+        self.bnE21 = nn.BatchNorm2d(128)
+        self.relu21 = nn.ReLU() 
         self.convE22 = nn.Conv2d(128, 128, 3, padding='same')
         nn.init.kaiming_normal_(self.convE22.weight)
         self.bnE22 = nn.BatchNorm2d(128)
         self.reluE22 = nn.ReLU() 
+        self.dropE2 = nn.Dropout(0.3)
+
+        self.upsamp1 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.convE11 = nn.Conv2d(128, 64, 3, padding='same')
+        nn.init.kaiming_normal_(self.convE11.weight)
+        self.bnE11 = nn.BatchNorm2d(128)
+        self.relu11 = nn.ReLU() 
+        self.convE12 = nn.Conv2d(64, 64, 3, padding='same')
+        nn.init.kaiming_normal_(self.convE12.weight)
+        self.bnE12 = nn.BatchNorm2d(64)
+        self.reluE12 = nn.ReLU() 
+        self.dropE1 = nn.Dropout(0.3)
         
-        self.upsamp2 = tf.keras.layers.UpSampling2D(name='upsamp2')
-        self.upconvE2 = tf.keras.layers.Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal', name='upconvE2')
-        self.convE21 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal', name='convE21')
-        self.convE22 = tf.keras.layers.Conv2D(128, 3, activation=None, padding='same', kernel_initializer='he_normal', name='convE22')
-        self.batchE2 = tf.keras.layers.BatchNormalization(name='batchE2') 
-        self.actE2 = tf.keras.layers.Activation('relu', name='actE2')
-         
-        self.upsamp1 = tf.keras.layers.UpSampling2D(name='upsamp1')
-        self.upconvE1 = tf.keras.layers.Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal', name='upconvE1')
-        self.convE11 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal', name='convE11')
-        self.convE12 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal', name='convE12')
-        self.convE13 = tf.keras.layers.Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal', name='convE13')
-        self.out = tf.keras.layers.Conv2D(1, 1, activation='sigmoid', name='out')
+        self.convOut1 = nn.Conv2d(64, 2, 3, padding='same')
+        self.reluOut1 = nn.ReLU() 
+        self.convOut2 = nn.Conv2d(2, 1, 1, padding='same')
+        self.sigmoid = nn.Sigmoid()
+        
     
-    def call(self, inputs):
-        x = self.convC11(inputs)
+    def call(self, x):
+        x = self.convC11(x)
+        x = self.bnC11(x)        
+        x = self.reluC11(x)        
         x = self.convC12(x)
-        x = self.batchC1(x)
-        x_cat1 = self.actC1(x)
+        x = self.bnC12(x)
+        x_cat1 = self.reluC12(x)        
         x = self.dropC1(x_cat1)
         x = self.poolC1(x)
-
+        
         x = self.convC21(x)
+        x = self.bnC21(x)        
+        x = self.reluC21(x)        
         x = self.convC22(x)
-        x = self.batchC2(x)
-        x_cat2 = self.actC2(x)
+        x = self.bnC22(x)
+        x_cat2 = self.reluC22(x)        
         x = self.dropC2(x_cat2)
         x = self.poolC2(x)
         
         x = self.convC31(x)
+        x = self.bnC31(x)        
+        x = self.reluC31(x)        
         x = self.convC32(x)
-        x = self.batchC3(x)
-        x_cat3 = self.actC3(x)
+        x = self.bnC32(x)
+        x_cat3 = self.reluC32(x)        
         x = self.dropC3(x_cat3)
         x = self.poolC3(x)
         
         x = self.convC41(x)
+        x = self.bnC41(x)        
+        x = self.reluC41(x)        
         x = self.convC42(x)
-        x = self.dropC4(x)
+        x = self.bnC42(x)
+        x_cat4 = self.reluC42(x)        
+        x = self.dropC4(x_cat4)
+        x = self.poolC4(x)
+
+        x = self.convC51(x)
+        x = self.bnC51(x)
+        x = self.reluC51(x)
+        x = self.convC52(x)
+        x = self.bnC52(x)
+        x = self.reluC52(x)
+        x = self.dropC5(x)
+
+        # EXPANDING PATH
+        # cat and then upsamp
+        x = self.upsamp4(x)
+        x = torch.cat((x_cat4, x), dim=1)
+        x = self.convE41(x)
+        x = self.bnE41(x)
+        x = self.reluE41(x)
+        x = self.convE42(x)
+        x = self.bnE42(x)
+        x = self.reluE42(x)
+        x = self.dropE4(x)
 
         x = self.upsamp3(x)
-        x = self.upconvE3(x)
-        x = tf.concat([x_cat3, x], axis=3)
+        x = torch.cat((x_cat3, x), dim=1)
         x = self.convE31(x)
+        x = self.bnE31(x)
+        x = self.reluE31(x)
         x = self.convE32(x)
-        x = self.batchE3(x)
-        x = self.actE3(x)
+        x = self.bnE32(x)
+        x = self.reluE32(x)
+        x = self.dropE3(x)
 
         x = self.upsamp2(x)
-        x = self.upconvE2(x)
-        x = tf.concat([x_cat2, x], axis=3)
+        x = torch.cat((x_cat2, x), dim=1)
         x = self.convE21(x)
+        x = self.bnE21(x)
+        x = self.reluE21(x)
         x = self.convE22(x)
-        x = self.batchE2(x)
-        x = self.actE2(x)
-        
+        x = self.bnE22(x)
+        x = self.reluE22(x)
+        x = self.dropE2(x)
+
         x = self.upsamp1(x)
-        x = self.upconvE1(x)
-        x = tf.concat([x_cat1, x], axis=3)
+        x = torch.cat((x_cat1, x), dim=1)
         x = self.convE11(x)
+        x = self.bnE11(x)
+        x = self.reluE11(x)
         x = self.convE12(x)
-        x = self.convE13(x)
-        x = self.out(x)
+        x = self.bnE12(x)
+        x = self.reluE12(x)
+        x = self.dropE1(x)
+
+        x = self.convOut1(x)
+        x = self.reluOut1(x)
+        x = self.convOut2(x)
+        x = self.sigmoid(x)
         
         return x    
     
+    # TODO: remove
     def build_model(input_size):
         inputs = tf.keras.layers.Input(input_size)
         model = UNet()
@@ -180,3 +211,8 @@ class UNet(nn.Module):
         model = tf.keras.Model(inputs = inputs, outputs = outputs)
         return model    
     
+if __name__ == "__main__":
+    ao = torch.zeros((16, 96, 248, 1))
+    ao = torch.permute(ao, (0, 3, 1, 2))
+    model = unet()
+    ao_out = model(ao)
