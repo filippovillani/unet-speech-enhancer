73928a00998d739d25928477917143e35bd1a9b8
filippovillani <filippo.villani.1996@gmail.com>
2023-05-03 23:47:23 +0200

    train refactored into class

--- config.py
+++ config.py
@@ -1,50 +1,84 @@
-from pathlib import Path
-from argparse import Namespace
 import os
-import torch 
+import random
+import sys
+from argparse import Namespace
+from pathlib import Path
 
-def create_hparams():
-    hparams = Namespace(batch_size = 4,
-                        epochs = 50,
-                        patience = 20,
-                        lr = 1e-3,
-                        sr = 16000,
-                        n_mels = 96,
-                        n_fft = 1024,
-                        n_channels = 1,
-                        hop_len = 256,
-                        audio_ms = 4080,
-                        min_noise_ms = 1000)
+import numpy as np
+import torch
+
+
+def create_hparams():   
     
-    audio_len_ = int(hparams.sr * hparams.audio_ms / 1000)
-    frame_len_ = int(audio_len_ // hparams.hop_len + 1)
-    hparams = Namespace(**vars(hparams),
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    training_hparams = Namespace(batch_size = 1,
+                                 lr = 1e-4,
+                                 weights_decay = None,
+                                 epochs = 30,
+                                 patience = 10,
+                                 lr_patience = 3,
+                                 loss = "l1", # can be one of ["l1", "complexmse", "mse", "frobenius"]
+                                 max_snr_db = 12,
+                                 min_snr_db = -6) 
+                                 
+    model_hparams = Namespace(first_unet_channel_units = 32,
+                              unet_kernel_size = (3,3),
+                              drop_rate = 0.1,
+                              conv_channels = [32, 64, 128],
+                              conv_kernel_size = (5,3),
+                              degli_hidden_channels = 64,
+                              degli_kernel_size = (5,3),
+                              degli_data_lr = 1e-6)
+    
+    audio_hparams = Namespace(sr = 16000,
+                              n_mels = 80,
+                              n_fft = 1024,
+                              n_channels = 1,
+                              hop_len = 256,
+                              audio_ms = 1020, #4080
+                              audio_thresh = 0.1,
+                              min_noise_ms = 1000)
+    # Other useful audio parameters
+    audio_len_ = int(audio_hparams.sr * audio_hparams.audio_ms // 1000)
+    n_frames_ = int(audio_len_ // audio_hparams.hop_len + 1)
+    n_stft_ = int(audio_hparams.n_fft//2 + 1)
+    
+    hparams = Namespace(**vars(training_hparams),
+                        **vars(model_hparams),
+                        **vars(audio_hparams),
                         audio_len = audio_len_,
-                        frame_len = frame_len_)
+                        n_frames = n_frames_,
+                        n_stft = n_stft_,
+                        device = device,
+                        num_workers = 0)
     
     return hparams
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+def set_seeds(seed = 42):
+    
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
 
+# For reproducibility
+SEED = 42
+set_seeds(SEED)
+
+# Directories
 MAIN_DIR = Path(__file__).parent
+sys.path.append(MAIN_DIR)
+
 DATA_DIR = MAIN_DIR / "data"
 WEIGHTS_DIR = MAIN_DIR / "weights"
 RESULTS_DIR = MAIN_DIR / 'results'
 PREDICTION_DIR = MAIN_DIR / 'predictions'
 MIX_EX_DIR = MAIN_DIR / 'mixture_example'
 
-# Make directories
-if not os.path.exists(WEIGHTS_DIR):
-    os.mkdir(WEIGHTS_DIR)
+_dirs = [WEIGHTS_DIR, DATA_DIR, RESULTS_DIR, PREDICTION_DIR, MIX_EX_DIR]
 
-if not os.path.exists(RESULTS_DIR):
-    os.mkdir(RESULTS_DIR)
-    
-if not os.path.exists(DATA_DIR):
-    os.mkdir(DATA_DIR)
-    
-if not os.path.exists(PREDICTION_DIR):
-    os.mkdir(PREDICTION_DIR)
-
-if not os.path.exists(MIX_EX_DIR):
-    os.mkdir(MIX_EX_DIR)
+for dir in _dirs:
+    if not os.path.exists(dir):
+        os.mkdir(dir)
---
+++ train.py
@@ -0,0 +1,247 @@
+import argparse
+import os
+from time import time
+
+import torch
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+import config
+from dataset import build_dataloaders
+from metrics import SI_SSDR
+from model import UNet
+from save_plots import plot_train_hist
+from utils import (denormalize_db_spectr, load_config, load_json, save_config,
+                   save_json, to_linear)
+
+
+class Trainer:
+    def __init__(self, args):
+        
+        self.experiment_name = args.experiment_name
+        self._make_dirs(args.experiment_name, 
+                         args.resume_training,
+                         args.overwrite)
+        self._set_paths()
+        self._set_hparams(args.resume_training)
+        self._set_loss(self.hprms.loss)
+        
+        self.model = UNet().to(self.hprms.device)
+        self.optimizer = torch.optim.Adam(params=self.model.parameters(),
+                                          lr=self.hprms.lr)
+        self.lr_sched = ReduceLROnPlateau(self.optimizer, 
+                                          factor=0.5, 
+                                          patience=self.hprms.lr_patience)
+        
+        self.training_state = {"epochs": 0,
+                                "patience_epochs": 0,  
+                                "best_epoch": 0,
+                                "best_epoch_score": 0,
+                                "train_hist": {},
+                                "val_hist": {}}
+        
+        if args.resume_training:
+            self.model.load_state_dict(torch.load(self.ckpt_weights_path))
+            self.optimizer.load_state_dict(torch.load(self.ckpt_opt_path))
+            self.lr_sched.load_state_dict(torch.load(self.ckpt_sched_path))
+            self.training_state = load_json(self.training_state_path) 
+       
+        self.sissdr = SI_SSDR()
+    
+    
+    def train(self, train_dl, val_dl):
+        
+        print('_____________________________')
+        print('       Training start')
+        print('_____________________________')
+        
+        while self.training_state["patience_epochs"] < self.hprms.patience and self.training_state["epochs"] < self.hprms.epochs:
+            
+            self.training_state["epochs"] += 1 
+            print(f'\n§ Train Epoch: {self.training_state["epochs"]}\n')
+            
+            self.model.train()
+            train_scores = {"loss": 0.,
+                            "si-ssdr": 0.}
+            start_epoch = time()        
+            pbar = tqdm(train_dl, desc=f'Epoch {self.training_state["epochs"]}', postfix='[]')
+        
+            for n, batch in enumerate(pbar):   
+                    
+                self.optimizer.zero_grad()  
+                
+                noisy_speech, clean_speech = batch["noisy"].float().to(self.hprms.device), batch["speech"].float().to(self.hprms.device)
+                enhanced_speech = self.model(noisy_speech)
+                loss = self.loss_fn(enhanced_speech, clean_speech)
+    
+                if (not torch.isnan(loss) and not torch.isinf(loss)): 
+                    train_scores["loss"] += ((1./(n+1))*(loss-train_scores["loss"]))
+    
+                loss.backward()  
+                self.optimizer.step() 
+                
+                sdr_metric = self.sissdr(to_linear(denormalize_db_spectr(enhanced_speech)),
+                                            to_linear(denormalize_db_spectr(noisy_speech))).detach()
+                
+                if (not torch.isnan(sdr_metric) and not torch.isinf(sdr_metric)):
+                    train_scores["si-ssdr"] += ((1./(n+1))*(sdr_metric-train_scores["si-ssdr"]))
+                    
+                scores_to_print = str({k: round(float(v), 4) for k, v in train_scores.items()})
+                pbar.set_postfix_str(scores_to_print)
+
+                # if n == 10:
+                    # break  
+                 
+            val_scores = self.eval_model(model = self.model, 
+                                         test_dl = val_dl)
+            
+            self.lr_sched.step(val_scores["si-ssdr"])
+            
+            self._update_training_state(train_scores, val_scores)
+            self._save_training_state()
+            plot_train_hist(self.training_state_path)
+            
+            print(f'Epoch time: {int(((time()-start_epoch))//60)} min {int(((time()-start_epoch))%60)} s')
+            print('_____________________________')
+
+        print("____________________________________________")
+        print("          Training completed")    
+        print("____________________________________________")
+
+        return self.training_state
+             
+             
+             
+    def eval_model(self,
+                   model: torch.nn.Module, 
+                   test_dl: DataLoader)->dict:
+
+        model.eval()
+
+        test_scores = {"loss": 0.,
+                        "si-ssdr": 0.}        
+        pbar = tqdm(test_dl, desc=f'Evaluation', postfix='[]')
+        with torch.no_grad():
+            for n, batch in enumerate(pbar):   
+                
+                noisy_speech, clean_speech = batch["noisy"].float().to(self.hprms.device), batch["speech"].float().to(self.hprms.device)
+                enhanced_speech = self.model(noisy_speech)
+                
+                loss = self.loss_fn(enhanced_speech, clean_speech)
+                    
+                test_scores["loss"] += ((1./(n+1))*(loss-test_scores["loss"]))
+                
+                sdr_metric = self.sissdr(to_linear(denormalize_db_spectr(enhanced_speech)),
+                                         to_linear(denormalize_db_spectr(clean_speech)))
+                test_scores["si-ssdr"] += ((1./(n+1))*(sdr_metric-test_scores["si-ssdr"]))  
+                 
+                scores_to_print = str({k: round(float(v), 4) for k, v in test_scores.items()})
+                pbar.set_postfix_str(scores_to_print)
+                
+                # if n == 10:
+                #     break  
+                 
+        return test_scores  
+    
+    
+    def _make_dirs(self, experiment_name, resume_training, overwrite):
+        
+        experiment_dir = config.RESULTS_DIR / self.experiment_name
+        weights_dir = config.WEIGHTS_DIR / self.experiment_name
+        
+        if not os.path.exists(experiment_dir):
+            os.mkdir(experiment_dir)
+        else:
+            if not resume_training and not overwrite:
+                raise UserWarning('To overvwrite the current experiment use the --overwrite flag')
+        
+        if not os.path.exists(weights_dir):
+            os.mkdir(weights_dir)    
+            
+                           
+    def _set_paths(self):
+        
+        experiment_dir = config.RESULTS_DIR / self.experiment_name
+        self.training_state_path = experiment_dir / "train_state.json"
+        self.config_path = experiment_dir / 'config.json'
+        
+        weights_dir = config.WEIGHTS_DIR / self.experiment_name
+        self.best_weights_path = weights_dir / 'best_weights'
+        self.ckpt_weights_path = weights_dir / 'ckpt_weights'
+        self.ckpt_opt_path = weights_dir / 'ckpt_opt'
+        self.ckpt_sched_path = weights_dir / 'ckpt_sched'
+        
+        
+    def _set_hparams(self, resume_training):
+        
+        ''' Creates the hyperparameters if it's the first time,
+        otherwise loads the hyperparameters from config.json '''
+        
+        if resume_training:
+            self.hprms = load_config(self.config_path)
+        else:
+            self.hprms = config.create_hparams()
+            save_config(self.hprms, self.config_path)
+            
+            
+    def _set_loss(self, loss: str):
+   
+        if loss == "l1":
+            self.loss_fn = torch.nn.L1Loss()
+        elif loss == "mse":
+            self.loss_fn = torch.nn.MSELoss()
+        
+        
+    def _update_training_state(self, train_scores, val_scores):
+
+        train_scores = {k: float(v) for k, v in train_scores.items()}
+        val_scores = {k: float(v)  for k, v in val_scores.items()}
+        
+        for key, value in train_scores.items():
+            if key not in self.training_state["train_hist"]:
+                self.training_state["train_hist"][key] = []
+            self.training_state["train_hist"][key].append(value)
+        
+        for key, value in val_scores.items():
+            if isinstance(value, torch.Tensor):
+                value = value.item()
+            if key not in self.training_state["val_hist"]:
+                self.training_state["val_hist"][key] = []
+            self.training_state["val_hist"][key].append(value)
+            
+            
+    def _save_training_state(self):
+        # Save the best model
+        if self.training_state["patience_epochs"] == 0:
+            torch.save(self.model.state_dict(), self.best_weights_path)
+                
+        # Save checkpoint to resume training
+        save_json(self.training_state, self.training_state_path)  
+        torch.save(self.model.state_dict(), self.ckpt_weights_path)
+        torch.save(self.optimizer.state_dict(), self.ckpt_opt_path)
+        torch.save(self.lr_sched.state_dict(), self.ckpt_sched_path)
+        
+        
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--experiment_name', 
+                        type=str, 
+                        help='Choose a name for your experiment',
+                        default='test00') 
+    
+    parser.add_argument('--resume_training',
+                        action='store_true',
+                        help="use this flag if you want to restart training from a checkpoint")
+    
+    parser.add_argument('--overwrite',
+                        action='store_false',
+                        help="use this flag if you want to overwrite an experiment")
+    
+    args = parser.parse_args()
+    
+    trainer = Trainer(args)
+    train_dl, val_dl, _ = build_dataloaders(config.DATA_DIR, trainer.hprms) 
+    trainer.train(train_dl, val_dl)
