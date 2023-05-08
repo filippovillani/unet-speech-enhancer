0472883968a52424ef7e3e31a79a59939a7a518b
filippovillani <filippo.villani.1996@gmail.com>
2023-05-08 16:49:27 +0200

    predict implemented

--- src/predict.py
+++ src/predict.py
@@ -1,37 +1,80 @@
-import numpy as np
-import soundfile as sf
-import tensorflow as tf
-import librosa 
+import argparse
+
+import torch
+from librosa.filters import mel
 
 import config
+from networks.PInvDAE.models import PInvDAE
 from networks.UNet.models import UNet
-from utils.audioutils import melspectrogram, inverse_spectrogram
-from utils.plots import plot_spectrograms
+from utils.audioutils import (denormalize_db_spectr, normalize_db_spectr,
+                              open_audio, save_audio, standardization, to_db,
+                              to_linear)
+from utils.utils import load_config
+
 
 def predict(args):
-    # TODO: to pytorch
-    weights_path = config.WEIGHTS_DIR / args.weights_dir / args.weights_dir
-    audio_path = config.MIX_EX_DIR / args.audio_path
-    output_path = config.PREDICTION_DIR / audio_path.name.replace(".wav", f"_prediction_{args.weights_dir}.wav")
-    fig_path = config.PREDICTION_DIR / audio_path.name.replace(".wav", f"_{args.weights_dir}.png")
-    # Compute input spectrogram, make the prediction and save waveform
-    audio, _ = librosa.load(str(audio_path), sr=16000) 
-    spectr = tf.expand_dims(melspectrogram(audio), axis=0)
-    # Initialize the model and load weights
-    model = UNet().double().to(config.device)
-    # TODO: load torch weights
-    model.load_weights(weights_path)
+    
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    noisy_speech_path = config.MIX_EX_DIR / args.audio
+    enh_speech_path = config.MIX_EX_DIR / args.audio.replace('.wav', '_enh.wav')
+    enh_weights_path = config.MODELS_DIR / args.enhancer / 'weights' / 'best_weights'
+    enh_config_path = config.MODELS_DIR / args.enhancer / 'config.json'
+    pinv_weights_path = config.MODELS_DIR / args.pinv / 'weights' / 'best_weights'
+    pinv_config_path = config.MODELS_DIR / args.pinv / 'config.json'
+    
+    enh_hparams = load_config(enh_config_path)
+    enh_model = UNet(enh_hparams).to(device)
+    enh_model.load_state_dict(torch.load(enh_weights_path))
+    
+    pinv_hparams = load_config(pinv_config_path)
+    pinv_model = PInvDAE(pinv_hparams).to(device)
+    pinv_model.load_state_dict(torch.load(pinv_weights_path))
+    
+    noisy_wav = open_audio(noisy_speech_path, pinv_hparams.sr)
+    noisy_wav = standardization(noisy_wav)
+    noisy_stft = torch.stft(noisy_wav,
+                            n_fft=pinv_hparams.n_fft,
+                            hop_length=pinv_hparams.hop_len,
+                            window = torch.hann_window(pinv_hparams.n_fft),
+                            return_complex=True).to(device)
+    melfb = torch.as_tensor(mel(sr = pinv_hparams.sr, 
+                                n_fft = pinv_hparams.n_fft, 
+                                n_mels = pinv_hparams.n_mels)).to(device)
+    
+    noisy_melspec = normalize_db_spectr(to_db(torch.matmul(melfb, 
+                                                           (torch.abs(noisy_stft)**2).float()), 
+                                              power_spectr=True)).unsqueeze(0).unsqueeze(0)
+    
+    with torch.no_grad():
+        
+        enh_melspec = enh_model(noisy_melspec)
+        enh_stftspec = pinv_model(enh_melspec).squeeze(1)
+        enh_stftspec = to_linear(denormalize_db_spectr(enh_stftspec))
+        enh_stft = enh_stftspec * torch.exp(1j * torch.angle(noisy_stft))
+        enh_speech_wav = torch.istft(enh_stft,
+                                     n_fft = pinv_hparams.n_fft,
+                                     window = torch.hann_window(pinv_hparams.n_fft).to(enh_stft.device)).squeeze()
 
-    # spectr = tf.expand_dims(spectrogram(audio), axis=0)
-    enhanced_speech_spectr = model(spectr, training=False)
-    enhanced_speech_spectr = tf.squeeze(enhanced_speech_spectr)
-    # Reconstruct the time-domain signal
-    enhanced_speech = inverse_spectrogram(enhanced_speech_spectr)
-    enhanced_speech = enhanced_speech / np.max(enhanced_speech)
+    save_audio(enh_speech_wav, enh_speech_path)
     
-    # Computed spectrogram of inverse_spectrogram of enhanced_speech for plot
-    reconstructed_spec = melspectrogram(enhanced_speech)
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
     
-    sf.write(output_path, enhanced_speech, samplerate=16000)
-    plot_spectrograms(tf.squeeze(spectr), enhanced_speech_spectr, tf.squeeze(reconstructed_spec), fig_path)
+    parser.add_argument('--enhancer', 
+                        type=str, 
+                        default='enhancer80_00') 
     
+    parser.add_argument('--pinv', 
+                        type=str, 
+                        default='pinvdae80_00') 
+    
+    parser.add_argument('--audio', 
+                        type=str,
+                        help="audio.wav in mixture_example/", 
+                        default='noisy0.wav') 
+    
+    args = parser.parse_args()
+    
+    predict(args)
